a43c9cc36090c92916c17575a54214e6c756ed32
Akshita-Joshi <joshiakki321@gmail.com>
2021-06-21 19:22:29 +0530

    auth

---
+++ lib/authentication/authenticate.dart
@@ -0,0 +1,72 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:mentor_mate/authentication/login.dart';
+import 'package:mentor_mate/home.dart';
+
+class Authenticate extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_auth.currentUser != null) {
+      return StudentHome();
+    } else {
+      return Login();
+    }
+  }
+}
+
+Future<User> createAccount(String email, String password) async {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  try {
+    User user = (await _auth.createUserWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+
+    if (user != null) {
+      print("Account Created Successful");
+      return user;
+    } else {
+      print("Account Creation Failed");
+      return user;
+    }
+  } catch (e) {
+    print(e);
+    return null;
+  }
+}
+
+Future<User> logIn(String email, String password) async {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  try {
+    User user = (await _auth.signInWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+
+    // ignore: unnecessary_null_comparison
+    if (user != null) {
+      print("Login Successful");
+      return user;
+    } else {
+      print("Login Failed");
+      return user;
+    }
+  } catch (e) {
+    print(e);
+    return null;
+  }
+}
+
+Future logOut(BuildContext context) async {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  try {
+    await _auth.signOut().then((value) {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => Login()));
+    });
+  } catch (e) {
+    print("error");
+  }
+}
